00cadcba72451811c7f3af61ddc3abea32df4bd1
Greg Leeper <gleeper49@gmail.com>
2023-09-05 14:52:37 -0400

    temp commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,48 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { Faves } from './faves';
+import { FavesManager } from './faves';
 
-const faves = new Faves();
+const faves = new FavesManager();
+
+function uriExecutor(handler: (u: vscode.Uri) => void): (u: vscode.Uri | undefined) => void {
+  return (u: vscode.Uri | undefined) => {
+    executeOnUri(handler, u);
+  };
+}
+
+function executeOnUri(handler: (u: vscode.Uri) => void, uri?: vscode.Uri): void {
+  if (!uri) {
+    uri = vscode.window.activeTextEditor?.document.uri;
+  }
+  if (!uri) {
+    vscode.window.showErrorMessage("Unable to get file URI");
+    return;
+  }
+  if (uri.scheme !== "file") {
+    vscode.window.showErrorMessage("Currently, only file resources are supported");
+    return;
+  }
+  handler(uri);
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	context.subscriptions.push(vscode.commands.registerCommand('faves.add', (fileUri: vscode.Uri) => {
-    faves.add(fileUri);
-	}));
-
-  context.subscriptions.push(vscode.commands.registerCommand('faves.remove', (fileUri: vscode.Uri) => {
-    faves.remove(fileUri);
-  }));
-
-  context.subscriptions.push(vscode.commands.registerCommand('faves.toggle', (fileUri: vscode.Uri) => {
-    faves.toggle(fileUri);
-  }));
+	context.subscriptions.push(vscode.commands.registerCommand('faves.add', uriExecutor((u: vscode.Uri) => (faves.add(u)))));
+  context.subscriptions.push(vscode.commands.registerCommand('faves.remove', uriExecutor((u: vscode.Uri) => (faves.remove(u)))));
+  context.subscriptions.push(vscode.commands.registerCommand('faves.toggle', uriExecutor((u: vscode.Uri) => (faves.toggle(u)))));
 
   context.subscriptions.push(vscode.commands.registerCommand('faves.search', () => {
     faves.select();
   }));
+
+  vscode.workspace.onDidChangeConfiguration(e => {
+    if (e.affectsConfiguration("faves")) {
+      faves.reload();
+    }
+  });
 }
 
 // This method is called when your extension is deactivated
--- src/faves.ts
+++ src/faves.ts
@@ -1,39 +1,80 @@
 import { basename, dirname } from 'path';
 import * as vscode from 'vscode';
 
-export class Faves {
+interface Fave {
+  path: string;
+};
 
-  faves: Set<vscode.Uri>;
+interface FaveQuickPickItem  extends vscode.QuickPickItem, Fave {};
+
+// Note: we use a function (rather than having Fave extend QuickPickItem),
+// to ensure we don't write unnecessary data to settings
+function faveToQuickPick(fave: Fave): FaveQuickPickItem {
+  return {
+    ...fave,
+    label: basename(fave.path),
+    description: dirname(fave.path),
+  };
+}
+
+interface FavesConfiguration {
+  favorites: Fave[];
+};
+
+export class FavesManager {
+
+  faves: Map<string, Fave>;
+  eventEmitter: vscode.EventEmitter<void> = new vscode.EventEmitter<void>()
 
   constructor() {
-    this.faves = new Set<vscode.Uri>();
+    this.faves = new Map<string, Fave>();
+    this.reload();
   }
 
-  add(f: vscode.Uri) {
-    this.faves.add(f);
+  async add(f: vscode.Uri): Promise<void> {
+    this.faves.set(this.pth(f), {path: this.pth(f)});
+    return await this.updateConfiguration();
   }
 
-  remove(f: vscode.Uri) {
-    this.faves.delete(f);
+  async remove(f: vscode.Uri): Promise<void> {
+    this.faves.delete(this.pth(f));
+    return await this.updateConfiguration();
+  }
+
+  pth(f: vscode.Uri): string {
+    return f.path;
   }
 
   toggle(f: vscode.Uri) {
-    if (this.faves.has(f)) {
+    if (this.faves.has(this.pth(f))) {
       this.remove(f);
     } else {
       this.add(f);
     }
   }
 
-  select() {
-    vscode.window.showQuickPick(
-      Array.from(this.faves).map(uri => ({
-        label: basename(uri.path),
-        description: dirname(uri.path),
-      })),
-      {
-        canPickMany: false,
-      },
-    );
+  orderedFaves(): Fave[] {
+    return Array.from(this.faves.values()).sort((a, b) => a < b ? -1 : 1);
+  }
+
+  async select() {
+    const item = await vscode.window.showQuickPick(this.orderedFaves().map(faveToQuickPick));
+    if (!item) {
+      return;
+    }
+    vscode.window.showInformationMessage(`Picked item: ${item.path}`);
+  }
+
+  reload(): void {
+    const config = vscode.workspace.getConfiguration("faves", vscode.window.activeTextEditor?.document.uri);
+    const favorites = config.get<Fave[]>("favorites");
+    this.faves = new Map(favorites?.map(fave => [fave.path, fave]));
+  }
+
+  private updateConfiguration(): Promise<void> {
+    vscode.workspace.getConfiguration("faves").update("favorites", this.orderedFaves(), vscode.ConfigurationTarget.Workspace, true).then(undefined, (reason: any) => {
+      vscode.window.showInformationMessage(`Failed to update favorites: ${reason}`);
+    });
+    return Promise.resolve();
   }
 }

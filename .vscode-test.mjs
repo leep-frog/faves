4233fcb4433b4b9603d29f0645ea2b662e54d719
Greg Leeper <gleeper49@gmail.com>
2025-02-18 23:21:43 -0500

    fix tests

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from '@vscode/test-cli';
 import path from 'path';
 
+
 export default defineConfig({
 	files: 'out/test/**/*.test.js',
   workspaceFolder: path.resolve("src", "test", "test-workspace"),
@@ -9,5 +10,12 @@ export default defineConfig({
   },
   mocha: {
     timeout: 60000,
+  },
+
+  // TODO: Eventually remove the following:
+  // Tests stopped working with latest version, so pinned to this version for now
+  version: "1.96.4",
+  useInstallation: {
+    fromMachine: true,
   },
 });
